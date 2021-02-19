d0a3e376c44031b46f6f1fd6cdd7af93faf4f904
abai_web <abai.org@gmail.com>
2021-02-19 18:26:28 +0300

    Main work with Form, input, button, labels

--- KeremetForms/Form1.Designer.cs
+++ KeremetForms/Form1.Designer.cs
@@ -29,20 +29,71 @@ namespace KeremetForms
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblFound = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(273, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите ИНН клиента";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSearch.Location = new System.Drawing.Point(542, 113);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Поиск";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtInput.Location = new System.Drawing.Point(225, 111);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(311, 26);
+            this.txtInput.TabIndex = 2;
+            // 
+            // lblFound
+            // 
+            this.lblFound.AutoSize = true;
+            this.lblFound.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblFound.Location = new System.Drawing.Point(225, 201);
+            this.lblFound.Name = "lblFound";
+            this.lblFound.Size = new System.Drawing.Size(0, 24);
+            this.lblFound.TabIndex = 3;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblFound);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label1);
             this.Name = "FormMain";
             this.Text = "Keremet Bank";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblFound;
     }
 }
 
